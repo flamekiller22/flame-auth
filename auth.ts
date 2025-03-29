ef2aa9571f30f0d0510a9a620fa3b200a313db5b
flamekiller22 <flamekillerace@gmail.com>
2025-03-30 03:25:51 +0530

    Attempt adapter issue fix

--- auth.ts
+++ auth.ts
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
-import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 import authConfig from "@/auth.config";
 import { db } from "@/lib/db";
