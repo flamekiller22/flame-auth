6029b78206df60cc2035ba3efefb77fd947c4e3c
flamekiller22 <flamekillerace@gmail.com>
2025-03-30 03:22:51 +0530

    Update Resend Email to new domain

--- lib/mail.ts
+++ lib/mail.ts
@@ -3,30 +3,24 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 const domain = process.env.NEXT_PUBLIC_SITE_URL;
 
-export const sendPasswordResetEmail = async (
-  email: string,
-  token: string
-) => {
+export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `${domain}/auth/new-password?token=${token}`;
 
   await resend.emails.send({
-    from: "Auth - flamekiller <no-reply@flamekiller.org>",
+    from: "Auth - flamekiller <no-reply@auth.fk22.dev>",
     to: email,
     subject: "Reset your password",
     html: `<p>Click <a href="${resetLink}">here</a> to reset your password.</p>`,
-  })
-}
+  });
+};
 
-export const sendVerificationEmail = async (
-  email: string,
-  token: string
-) => {
+export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
   await resend.emails.send({
-    from: "Auth - flamekiller <no-reply@flamekiller.org>",
+    from: "Auth - flamekiller <no-reply@auth.fk22.dev>",
     to: email,
     subject: "Confirm your email",
     html: `<p>Click <a href="${confirmLink}">here</a> to confirm your email.</p>`,
-  })
-}
+  });
+};
